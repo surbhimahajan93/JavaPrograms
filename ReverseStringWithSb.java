4c19df983db5538c6b0948c6b7ee7a4206205a83
surbhimahajan93 <surbhimahajan93@gmail.com>
2020-08-01 06:10:30 +0000

    ReverseStringWithSb

--- ReverseStringWithSb.java
+++ ReverseStringWithSb.java
@@ -3,11 +3,16 @@ class ReverseStringWithSb{
      public static void main(String[] args){
          String input ="My name is Surbhi";
          System.out.println(reverse(input));
+          System.out.println(reverse2(input));
     }
 
     public static String reverse(String s){
         StringBuilder sb = new StringBuilder(s);
         sb.reverse();
         return sb.toString();
+    }
+
+    public static String reverse2(String s){
+       return new StringBuilder(s).reverse().toString();
     }
 }
